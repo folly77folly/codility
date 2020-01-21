d6f63cd1a2126e6f624802d91d943b444ebbbf06
aqim <folly77folly@gmail.com>
2020-01-21 11:25:54 +0100

    updated missing integer

--- MissingInteger.py
+++ MissingInteger.py
@@ -2,9 +2,11 @@ def solution(A):
     A.sort()
     last_number = A[-1]
     my_filtered_list = filter(lambda x : x > 0, A)
+    
     my_set = set(my_filtered_list)
     length_list = len(my_set)
     my_full_length_set = set(range(1,last_number))
+
     if last_number <= 0:
         return 1
     if length_list == last_number :
